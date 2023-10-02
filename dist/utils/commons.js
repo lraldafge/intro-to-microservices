826b084e4731bcd3008cbaa652a5ff6fa8f026b5
lraldafge <lralda@genesisempresarial.com>
2023-10-02 15:06:33 -0600

    removing dist

--- dist/utils/commons.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.passwordRegex = void 0;
-exports.passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
